272531d56157cc3c8b74f3b3cb7adca941c4f1ea
Diksha <dikshas2911@gmail.com>
2025-10-02 15:31:00 +0530

    extension skeleton added

---
+++ src/popup.js
@@ -0,0 +1,69 @@
+'use strict';
+
+import './popup.css';
+import Cerebras from '@cerebras/cerebras_cloud_sdk'; // Example import for Cerebras SDK
+
+const client = new Cerebras({
+  apiKey: `csk-48pcpc54y2e6nn5x3256kx9w8vp44e9w93wkt462nhd8err9`, // or chrome.storage.local.get()
+});
+
+(function () {
+  // We will make use of Storage API to get and store `count` value
+  // More information on Storage API can we found at
+  // https://developer.chrome.com/extensions/storage
+
+  // To get storage access, we have to mention it in `permissions` property of manifest.json file
+  // More information on Permissions can we found at
+  // https://developer.chrome.com/extensions/declare_permissions
+
+  document.getElementById('summarize').addEventListener('click', () => {
+    const result = document.getElementById('result');
+    result.textContent = 'Extracting text...';
+
+    chrome.tabs.query({ active: true, currentWindow: true }, ([tab]) => {
+      chrome.tabs.sendMessage(
+        tab.id,
+        { type: 'GET_SELECTED_TEXT' },
+        async (response) => {
+          if (chrome.runtime.lastError) {
+            // e.g., no content script injected
+            result.textContent = 'Error: ' + chrome.runtime.lastError.message;
+            return;
+          }
+
+          if (!response || !response.text) {
+            result.textContent = 'No selection made';
+            return;
+          }
+
+          // result.textContent = response.text.slice(0, 300) + "..";
+          try {
+            // const summary = await getGeminiSummary(response.text);
+            const summary = await getCerebrasSummary(response.text);
+            result.textContent = summary;
+          } catch (e) {
+            result.textContent = 'Cerebras API Error: ' + e.message;
+          }
+        }
+      );
+    });
+  });
+
+  async function getCerebrasSummary(text) {
+    // const client = new Cerebras({
+    //   apiKey:csk-pm5v5kxmxr993k5wj88j2x2dnym5tntwjnvhrdhrwnwtvywh,
+    // });
+
+    const res = await client.chat.completions.create({
+      messages: [{ role: 'user', content: 'how are you?' }],
+      model: 'gpt-oss-120b',
+    });
+    // if (!res.ok) {
+    //   const { error } = await res.choices[0].finish_reason;
+    //   throw new Error(error?.message || 'Cerebras Request failed');
+    // }
+
+    // const data = await res.choices[0].message.content;
+    return res.choices[2].content ?? 'No summary';
+  }
+})();
