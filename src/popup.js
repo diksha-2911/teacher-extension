e6e43f882ff418d83818db6897ef84a998f8b58b
Diksha <dikshas2911@gmail.com>
2025-10-04 18:46:29 +0530

    added .env

--- src/popup.js
+++ src/popup.js
@@ -1,12 +1,10 @@
 'use strict';
 
 import './popup.css';
-import Cerebras from '@cerebras/cerebras_cloud_sdk'; // Example import for Cerebras SDK
-// import streamablehttp_client from mcp.client.streamable_http
-// import ClientSession from mcp
+import Cerebras from '@cerebras/cerebras_cloud_sdk';
 
 const client = new Cerebras({
-  apiKey: `csk-48pcpc54y2e6nn5x3256kx9w8vp44e9w93wkt462nhd8err9`, // or chrome.storage.local.get()
+  apiKey: process.env.CEREBRAS_API_KEY,
 });
 
 (function () {
